8a71a15c401da6172ccf5797777948708cd71434
sreeper <remigijus.sportlich@gmail.com>
2024-06-15 22:17:58 +0100

    Fix git tracking for nvim and split init.lua file

---
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,29 @@
+return {
+
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        kotlin = { 'ktlint' },
+        python = { 'ruff', 'mypy' },
+      }
+
+      -- Create autocommand which carries out the actual linting
+      -- on the specified events.
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+  },
+}
